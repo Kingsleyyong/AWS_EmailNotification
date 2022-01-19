d320b6d01c104b4e277eaa1b96c86f63e6263445
Kingsley <kingsleyyong0208@gmail.com>
2022-01-19 10:05:55 +0800

    FE Email Subscribe Interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Card, TextField, Button } from '@mui/material'
+import { useRef } from 'react'
 
 function App() {
+
+  let email = useRef(), name = useRef();
+
+  const handleSubscribeButton = () => {
+      console.log(email.current.value, name.current.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ 
+      margin: '-1vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh'
+    }}>
+      <Card style={{
+        textAlign: 'center',
+        backgroundColor: 'lightblue',
+        width: '50vw',
+        padding: '1.6vw'
+      }}>
+        <h1 style={{fontSize: '2.5vw'}}>Enter your email to subscribe us!</h1>
+        <Box
+          component="form"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <TextField
+            sx={{m: '1vw', width: '35vw'}}
+            required
+            id='email'
+            inputRef={email}
+            label='Email'
+          />
+
+          <TextField
+            sx={{m: '1vw', width: '35vw'}}
+            required
+            id='name'
+            inputRef={name}
+            label='Name'
+          />
+
+          <Button
+            variant="outlined"
+            sx={{ m: '1vw', width: '35vw' }}
+            onClick={()=>{handleSubscribeButton()}}
+          >
+            Subscribe
+          </Button>
+
+        </Box>
+      </Card>
     </div>
   );
 }
